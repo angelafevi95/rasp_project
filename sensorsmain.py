2a271e58a637916cdf486f661fd90580619106aa
angelafevi95 <angelafevi95@gmail.com>
2020-04-05 17:18:51 +0100

    test

--- sensorsmain.py
+++ sensorsmain.py
@@ -1,6 +1,8 @@
-
+from dht import *
+from mq2 import *
 from bht1750 import *
 
+## Sound noise measures
 bus = smbus.SMBus(1)
 sensor = bht1750(bus)
 
@@ -9,4 +11,22 @@ measure = sensor.measure_high_res()
 
 print ("Light Level ", measure)
 
-print ("Main done ")
+
+## Temperature and humidity measures
+# dht = dht()
+# hum, temp = dht.get_data()
+
+# print("HUmidity", hum, "%")
+# print("temperature", temp, "ªC")
+
+# ## Air quality measures 
+# mq = MQ()
+# listLPG, listCO, listSMOKE = mq.get_data()
+
+# print("LPG", listLPG)
+# print("CO", listCO)
+# print("SMOKE", listSMOKE)
+
+
+
+print("It's works!")
